a7bd511ea8145ddd87c2db92ceab42bf50f3f675
Nina Jade <jaderiver64@gmail.com>
2021-03-11 00:38:05 -0600

    updates

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Connect to database
 const db = new sqlite3.Database('./db/election.db', err => {
-  if (err) {
-    return console.error(err.message);
-  }
+    if (err) {
+        return console.error(err.message);
+    }
 
-  console.log('Connected to the election database.');
+    console.log('Connected to the election database.');
 });
 app.get('/', (req, res) => {
     res.json({
@@ -24,7 +24,9 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
     res.status(404).end();
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
